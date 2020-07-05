6507d4f41a488392dbecef9aa2a57ab2b806585b
apphow <apehow111@gmail.com>
2020-07-05 03:26:14 -0400

    Initial commit

---
+++ android/app/src/main/java/com/question_and_answer/MainActivity.java
@@ -0,0 +1,6 @@
+package com.question_and_answer;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Questions());
+}
+
+class Questions extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.grey.shade700,
+        body: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10.0),
+            child: QuestionPage(),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class QuestionPage extends StatefulWidget {
+  @override
+  _QuestionPageState createState() => _QuestionPageState();
+}
+
+class _QuestionPageState extends State<QuestionPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
